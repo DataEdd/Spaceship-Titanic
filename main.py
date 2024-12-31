cb37200329f4f7e450b270b72f6593319a6a1356
Aditya Joshi <adityajoshi408@gmail.com>
2024-12-30 19:59:43 -0600

    another setup test

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-print("hi")
-print("world")
-print("test")
-print("test23")
-print("test2223")
+import numpy as np  # type: ignore
+import pandas as pd # type: ignore
+
+train = pd.read_csv('Kaggle Comps/Spaceship Titanic/train-SpaceshipTitanic.csv')
+test = pd.read_csv('Kaggle Comps/Spaceship Titanic/test-SpaceshipTitanic.csv')
+
+#print(train.head())
+print(train.describe())
+
+
